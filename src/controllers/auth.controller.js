405ccf43ca21440dab2a431d9a6f84ef0118b33a
hunsrengking <hunsreng2021@email.com>
2025-09-25 17:07:22 +0700

    update

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,15 @@
+const AuthService = require("../services/auth.service");
+
+class AuthController {
+  async login(req, res, next) {
+    try {
+      const { username, password } = req.body;
+      const result = await AuthService.login({ username, password });
+      res.json(result);
+    } catch (error) {
+      next(error);
+    }
+  }
+}
+
+module.exports = new AuthController();
